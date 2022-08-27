3bfc101b4f28a9949ce4623fe2004fff67b0ac64
bekadev <bekapng@gmail.com>
2022-08-27 15:28:05 +0600

    27.08.2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ButtonCp} from './component/ButtonCP';
 
 function App() {
   return (
     <div className="App">
-      hi beka
+      <ButtonCp />
     </div>
   );
 }
